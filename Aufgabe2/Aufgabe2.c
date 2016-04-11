fe32190d09ad516a33a5a328138a3e6733a10815
Fabian Holtkötter <holtkoet@hm.edu>
2016-04-11 12:46:27 +0200

    Aufgab2.1 finished

--- Aufgabe2/Aufgabe2.c
+++ Aufgabe2/Aufgabe2.c
@@ -2,20 +2,25 @@
 #include <stdio.h>
 #include <time.h>
 
+
+
 int main(int argc, char *argv[]) {
 	struct timespec start, finish, requested;
 
-	requested.tv_sec = 0;
-	requested.tv_nsec = 1000;
-
 	clock_gettime(CLOCK_REALTIME, &start);
+
+	requested.tv_sec = start.tv_sec;
+	requested.tv_nsec = start.tv_nsec + 1000000;
+
 	clock_nanosleep(CLOCK_REALTIME,
-			0,
+			TIMER_ABSTIME,
 			&requested,
 			NULL);
 	clock_gettime(CLOCK_REALTIME, &finish);
 
 	printf("Waited %.3f msec\n", (start.tv_sec - finish.tv_sec) + ((finish.tv_nsec - start.tv_nsec) / 1000000.0));
+	printf("Start - Seconds: %d - NS: %d\n", start.tv_sec, start.tv_nsec);
+	printf("Finis - Seconds: %d - NS: %d\n", finish.tv_sec, finish.tv_nsec);
 
 	return EXIT_SUCCESS;
 }
