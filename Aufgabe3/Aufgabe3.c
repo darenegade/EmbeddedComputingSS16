6a8761e9acc3bb6fafac24c7cf37ffcdce34fc5d
Fabian Holtkötter <holtkoet@hm.edu>
2016-05-09 10:22:28 +0200

    aufg3 finished

--- Aufgabe3/Aufgabe3.c
+++ Aufgabe3/Aufgabe3.c
@@ -3,31 +3,43 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/**
+ * Magic Light-Show!!!
+ */
 int main(int argc, char *argv[]) {
+	// Check correct number of arguments
 	if(argc != 3){
 		printf("Please specify if 0 or 1 for LED0 and LED1\n");
 		return EXIT_FAILURE;
 	}
-	int led1 = atoi(argv[1]);
-	int led2 = atoi(argv[2]);
 
-	unsigned char bytemask = 0b00000011;
-
+	// try to open device file
 	int filedes = open("/dev/leds", O_RDWR);
 	if(filedes == -1){
 		printf("Can't open device.\n");
 		return EXIT_FAILURE;
 	}
 
-	printf("param1: %d - param2: %d", led1, led2);
-	const char data = bytemask & led1;
+	// determine states of leds
+	int led0 = atoi(argv[1]);
+	int led1 = atoi(argv[2]);
+	int led2 = led0 ^ led1;
+	int led3 = led0 | led1;
+	printf("led0: %d - led1: %d - led2: %d - led3: %d", led0, led1, led2, led3);
 
+	// create data bytemask
+	led1 = led1 << 1;
+	led2 = led2 << 2;
+	led3 = led3 << 3;
+	const char data = led0 | led1 | led2 | led3;
+
+	// write data bytemask to device
 	if(write(filedes, &data, 1) == -1){
 		printf("Can't write cause I'm stupid. errno is %d \n", errno);
 		return EXIT_FAILURE;
 	}
 
+	// close and exit
 	close(filedes);
-
 	return EXIT_SUCCESS;
 }
